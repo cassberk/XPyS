2554a2efbccd6a4a072f303bfb836d50858b8ab6
Cassidy Berk <cassberk@Cassidys-MacBook-Pro.local>
2021-02-07 10:26:39 -0800

    got rid of old derk object load section, it was commented for awhile, not just using hdf5

--- spectra.py
+++ spectra.py
@@ -31,24 +31,6 @@ import xps_peakfit.autofit.autofit
 import os
 import glob
 
-"""
-Load pre-defined models fitting
-"""
-
-def loadmodel(element):
-
-    f = open('/Volumes/GoogleDrive/My Drive/XPS/XPS_Library/xps/models/load_model_info.pkl', 'rb')   # 'r' for reading; can be omitted
-    load_dict = pickle.load(f)         # load file content as mydict
-    f.close() 
-
-    mod = lm.model.load_model(load_dict[element]['model_path'])
-    pars = pickle.load(open(load_dict[element]['params_path'],"rb"))
-    pairlist = load_dict[element]['pairlist']
-    element_ctrl = load_dict[element]['element_ctrl']
-
-    return mod, pars, pairlist, element_ctrl
-
-
 
 class spectra:
 
@@ -125,69 +107,7 @@ class spectra:
 
         self.bg_info = bg_info
         self.BE_adjust = BE_adjust
-        self.spectra_name = spectra_name
-        # self.prefixlist = [self.mod.components[i].prefix for i in range(len(self.mod.components))]   #Need this
-
-        if load_spectra_object == False:
-            return
-            # self.parent_sample = sample_object.sample_name
-            # self.orbital = orbital
-            # if spectra_name == None:
-            #     self.spectra_name = dc(self.parent_sample + '_' + orbital)
-            # else:
-            #     self.spectra_name = spectra_name + '_' + orbital
-
-            # self.carbon_adjust = carbon_adjust
-            
-            # if self.carbon_adjust:
-            #     print('Adjusted energy to carbon reference')
-            #     self.E = dc(sample_object.data[orbital]['energy']) - self.carbon_adjust
-            # else:
-            #     self.E = dc(sample_object.data[orbital]['energy'])
-
-            # self.I = dc(sample_object.data[orbital]['intensity'])
-
-            # self.isub = dc([[] for k in range(len(self.I))])
-            # self.bg = dc([[] for k in range(len(self.I))])
-            # self.bgpars = dc([[] for k in range(len(self.I))])
-            # self.area = dc([[] for k in range(len(self.I))])  
-            
-            # self.data = dc(sample_object.data[orbital])
-
-            # if load_model == False:
-            #     self.params_full = parameters
-            #     self.mod = model
-            #     self.pairlist = pairlist
-            #     self.element_ctrl = element_ctrl
-            # else:
-            #     self.mod, self.params_full, self.pairlist, self.element_ctrl = loadmodel(load_model)
-
-            # if type(self.params_full) == list:
-            #     self.params = dc(self.params_full[0])
-            # else:
-            #     self.params = dc(self.params_full)
-
-            # self.autofit = autofit
-            # self.leglist = ['Data','Fit Guess'] + [self.pairlist[i][0] for i in range(len(self.pairlist))]
-            # self.prefixlist = [self.mod.components[i].prefix for i in range(len(self.mod.components))]
-
-
-        elif load_spectra_object == True:
-
-            spectra_path = os.path.join('/Volumes/GoogleDrive/My Drive/XPS/XPS_Library/xps/samples',\
-                            sample_object,orbital)
-            f = open(os.path.join(spectra_path,'spectra_attributes.pkl'), 'rb')   
-            savedict_load = pickle.load(f)         
-            f.close() 
-
-            for key in savedict_load:
-                self.__dict__[key] = savedict_load[key]
-
-            self.mod = lm.model.load_model(os.path.join(spectra_path,self.spectra_name+'_model.sav'))
-
-            fit_result_path_list = [f for f in glob.glob(spectra_path+"/*.sav") \
-                if 'fit_result' in f]
-            self.fit_results = [lm.model.load_modelresult(fit_result_path_list[i]) for i in range(len(fit_result_path_list))]
+        self.spectra_name = spectra_name        
 
     def load_experiment_spectra_from_vamas(self, vamas_obj,orbital = None):
         print(orbital)
