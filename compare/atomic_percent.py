72c81e8c1ccf2f8158ef466c3210fb3c7bb88e33
Cassidy Berk <cassberk@Cassidys-MacBook-Pro.local>
2021-02-08 11:20:23 -0800

    Fixed compare plot to take mean of atomic percent

--- compare/atomic_percent.py
+++ compare/atomic_percent.py
@@ -25,13 +25,24 @@ def plot_atomic_percents(sample_list,idx = None, error = 'std', width = 0.8, spe
             sample[1].calc_atomic_percent(specify_spectra = spectra_list)
 
         for spectra in enumerate(spectra_list):
+            
+
+            if idx is None:
+                at_pct = 100*sample[1].__dict__[spectra[1]].atomic_percent.mean()
+                at_pct_err = 100*sample[1].__dict__[spectra[1]].atomic_percent.std()
+            else:
+                at_pct = 100*sample[1].__dict__[spectra[1]].atomic_percent[idx]
+                at_pct_err = 0
             if spectra[1] not in fig_legend:
                 fig_legend.append(spectra[1])
-                fig_list.append(ax.bar(sample[0]+spectra[0]*spectra_width, 100*sample[1].__dict__[spectra[1]].atomic_percent[i], spectra_width, \
-                    color = spectra_colors[spectra[1]]))
+
+
+                fig_list.append(ax.bar(sample[0]+spectra[0]*spectra_width, at_pct, spectra_width, yerr=at_pct_err,\
+                              error_kw=dict(lw=5, capsize=20, capthick=3), color = spectra_colors[spectra[1]]))
+                
             else:
-                ax.bar(sample[0]+spectra[0]*spectra_width, 100*sample[1].__dict__[spectra[1]].atomic_percent[i], spectra_width, \
-                    color = spectra_colors[spectra[1]])
+                ax.bar(sample[0]+spectra[0]*spectra_width, at_pct, spectra_width, yerr=at_pct_err,\
+                              error_kw=dict(lw=5, capsize=20, capthick=3), color = spectra_colors[spectra[1]])
 
 
     ax.tick_params(labelsize = 40)
