4c209700a09551d1a166ecaa3d41ce046bd52967
Cassidy Berk <cassberk@Cassidys-MacBook-Pro.local>
2021-02-15 18:36:42 -0800

    avg to hdf5 should be workign for surfce and depth profiles

--- avg.py
+++ avg.py
@@ -1,11 +1,25 @@
+import h5py
+import os
+import re
+import numpy as np
+import glob
+import json
+import os
+from IPython import embed as shell
+
+def dumper(obj):
+    try:
+        return obj.toJSON()
+    except:
+        return obj.tolist()
+
 
 
 def read_avg(filepath):
-    f = open(filepath,'r')
-    f.seek(0)
-    lines = [line for line in f.readlines() if line != '\n']
-    f.close()
-    
+
+    with open(filepath, encoding="utf8", errors='ignore') as f:
+        lines = [line for line in f.readlines() if line != '\n']
+
     print(filepath)
     AXESDICT = {}
     PROPERTIES = {}
@@ -13,55 +27,48 @@ def read_avg(filepath):
     DATA['I'] = []
     i=0
     while i < len(lines)-1:
-
-        if (lines[i][0] != ';') and (lines[i][0] != '\n'):
+        if 'Dump of DataSpace' in lines[i]:
+            PROPERTIES.update({'VGD_Location':lines[i].split(';Dump of DataSpace')[1].strip().replace("'",'')})
+            i+=1
+        elif (lines[i][0] != ';') and (lines[i][0] != '\n'):
 
             if '$PROPERTIES' in lines[i]:
                 i+=1
                 while (lines[i][0] !='$'):
 
                     if lines[i][0] != ';':
-
-#                         props = [k.strip() for k in re.split('=',lines[i].strip())]
-
-#                         if props[1][0] =="'":
-#                             PROPERTIES.update({props[0].split(':')[0].strip():eval(props[1])})
-#                         else:
-#                             PROPERTIES.update({props[0].split(':')[0].strip():props[1]})
-
                         props = [k.strip() for k in re.split('=|:',lines[i].strip())]
-            
                         if props[1] =='VT_BSTR':
-                            PROPERTIES.update({props[0].split(':')[0].strip():eval(props[2])})
+                            if props[0] == 'DS_EXT_SUPROPID_COMMENTS':
+                                comments = props[2].replace("'","")
+                                i+=1
+                                while lines[i][0] == ' ':
+                                    comments+=lines[i].replace("'","")
+                                    i+=1
+                                PROPERTIES.update({props[0].split(':')[0].strip():comments})
+                            else:
+                                PROPERTIES.update({props[0].split(':')[0].strip():props[2].replace("'","")})
+                                i+=1
                         elif props[1] =='VT_DATE':
                             props = [k.strip() for k in re.split('=',lines[i].strip())]
                             PROPERTIES.update({props[0].split(':')[0].strip():props[1]})
+                            i+=1
                         elif (props[1] =='VT_I4') or (props[1] =='VT_I2'):
                             PROPERTIES.update({props[0].split(':')[0].strip():int(props[2])})
-#                             print(props[1],props[2])
+                            i+=1
                         elif props[1] == 'VT_BOOL':
-#                             print(props[1],props[2])
                             PROPERTIES.update({props[0].split(':')[0].strip():bool(props[2])})
+                            i+=1
                         elif (props[1] =='VT_R4'):
                             PROPERTIES.update({props[0].split(':')[0].strip():np.float(props[2])})
+                            i+=1
                         else:
                             PROPERTIES.update({props[0].split(':')[0].strip():props[2]})
                             print(props[1],'is a not specified dat format')
-
-
-                    i+=1
+                            i+=1
+                    else:
+                        i+=1
                     
-#                 DATA['I'] = 
-                
-                
-#                 elif '$DATAAXES' in lines[i]:
-#                     print(lines[i].strip())
-#                     i+=1
-#                     while (lines[i][0] !='$'):
-#         #                 i+=1
-#                         if lines[i][0] != ';':
-#                             print(lines[i][0],lines[i].strip())
-#                         i+=1
 
             elif '$SPACEAXES' in lines[i]:
                 SPACEAXES = {}
@@ -78,12 +85,18 @@ def read_avg(filepath):
             elif '$AXISVALUE' in lines[i]:
 
                 AXVAL = [x.strip() for x in re.split(r'\b$AXISVALUE\b|\bDATAXIS\b|\bSPACEAXIS\b|\bLABEL\b|\bPOINT\b|\bVALUE\b|=|;',lines[i].strip()) if x.strip() not in ['$AXISVALUE','']]
-
                 try:
-                    AXESDICT[eval(AXVAL[2])].append(np.float(AXVAL[4]))
+                    if (eval(AXVAL[2]) == 'Etch Time') or (eval(AXVAL[2]) == 'Etch Level'):
+                        AXESDICT[eval(AXVAL[2])].append(np.float(AXVAL[4]))
+                    elif eval(AXVAL[2]) == 'Position':
+                        AXESDICT[eval(AXVAL[2])].append(eval(AXVAL[4]))
                 except:
-                    AXESDICT[eval(AXVAL[2])] = []
-                    AXESDICT[eval(AXVAL[2])].append(np.float(AXVAL[4]))
+                    if (eval(AXVAL[2]) == 'Etch Time') or (eval(AXVAL[2]) == 'Etch Level'):
+                        AXESDICT[eval(AXVAL[2])] = []
+                        AXESDICT[eval(AXVAL[2])].append(np.float(AXVAL[4]))
+                    elif eval(AXVAL[2]) == 'Position':
+                        AXESDICT[eval(AXVAL[2])] = []
+                        AXESDICT[eval(AXVAL[2])].append(eval(AXVAL[4]))
 
 
                 AXVAL[2]
@@ -128,29 +141,44 @@ def read_avg(filepath):
         print((int(SPACEAXES['1']['numPoints']),int(SPACEAXES['0']['numPoints'])),np.array(DATA).shape)
         print('AXES DO NOT MATCH DATA')
         return
-        
-
 
     return PROPERTIES, SPACEAXES, AXESDICT, DATA
                 
                 
 
                
-def avg_to_hdf5(sample_name,experiment_name,folder_path):
+def avg_to_hdf5(sample_name,experiment_name,filepath = None,force = False):
     
-    f = h5py.File(os.path.join(os.getcwd(),sample_name+'.hdf5'),'a')
+    if filepath == None:
+        filelist = glob.glob(os.getcwd()+'/*')
+    elif type(filepath)==list:
+        filelist = filepath
+    elif type(filepath) == str:
+        filelist = glob.glob(filepath+'/*')
+
+    if force == False:
+        hdf = h5py.File('/'+os.path.join(os.path.join(*filelist[0].split('/')[0:-1]),sample_name+'.hdf5'),'a')
+    elif force == True:
+        hdf = h5py.File('/'+os.path.join(os.path.join(*filelist[0].split('/')[0:-1]),sample_name+'.hdf5'),'w')
+ 
     
-    experiment_group = f.create_group(experiment_name)
+    experiment_group = hdf.require_group(experiment_name)
     element_scans = []
-    for fp in glob.glob(folder_path+'/*'):
+    all_scans = []
+    for fp in filelist:
         if '.avg' in fp:
+
             if 'Survey' in fp.split('/')[-1].split('.')[0]:
                 spectra = fp.split('/')[-1].split('.')[0].split()[1]
-                element_scans.append(spectra)
+                all_scans.append(spectra)
                 print(spectra)
             else:
                 spectra = fp.split('/')[-1].split('.')[0].split()[0]
-                element_scans.append(spectra)
+                if spectra =='Valence':
+                    all_scans.append(spectra)
+                else:
+                    element_scans.append(spectra)
+                    all_scans.append(spectra)
                 print(spectra)
             try:
                 experiment_group.attrs['data_path'] = fp
@@ -163,10 +191,8 @@ def avg_to_hdf5(sample_name,experiment_name,folder_path):
 
             PROPERTIES, SPACEAXES, AXESDICT, DATA = read_avg(fp)
 
-
-            experiment_group.create_group(spectra)
+            experiment_group.require_group(spectra)
             for props,val in PROPERTIES.items():
-    #             print(props,val,type(val))
                 experiment_group[spectra].attrs[props] = val
 
             experiment_group[spectra].create_dataset('E', data = DATA['E'])   
@@ -179,8 +205,9 @@ def avg_to_hdf5(sample_name,experiment_name,folder_path):
             experiment_group[spectra].attrs['SPACEAXES'] = json.dumps(SPACEAXES, default=dumper, indent=2)
         
         experiment_group.attrs['element_scans'] = element_scans
+        experiment_group.attrs['all_scans'] = all_scans
 
         
-    f.close()
+    hdf.close()
 
             
